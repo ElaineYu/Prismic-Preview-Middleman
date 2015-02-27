1b1877a7465d06d5d9a7b8ad5c9d4a9c781df042
Elaine Yu <elaine@monsoonco.com>
2015-02-26 21:27:36 -0800

    adds view helpers, prisimc helper and prismic faq module

---
+++ source/config_modules/faq_prismic.rb
@@ -0,0 +1,23 @@
+require 'source/config_modules/prismic_helpers'
+module FaqPrismic
+  def faq_prismic
+    api
+    response_faq = api
+     .form('everything')
+     .query('[[:d = at(document.type, "faq")]]')
+     .orderings('[my.faq.order]')
+     .submit(api.master_ref)
+
+    @faq = response_faq.results
+
+    # @faq.each do |doc|
+    #     puts "hey"
+    #   puts doc["faq.title"].value
+    #   puts doc["faq.order"].value
+    #   doc["faq.qa"].group_documents.each do |segment|
+    #     puts segment.fragments["question"].value
+    #     puts segment.fragments["answer"].value
+    #   end
+    # end
+  end
+end
